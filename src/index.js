262fd9bd0658d21bb5069ba818d1079272f6eba3
Zach Lasky <zacharylasky123@gmail.com>
2019-08-14 16:57:40 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
+
+export const store = createStore(reducer);
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
